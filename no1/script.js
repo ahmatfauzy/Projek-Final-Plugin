7e56cfaa0d6a940ff89e1ef36225607790134797
ahmatfauzy <ahmatf703@gmail.com>
2024-07-30 00:03:10 +0700

    update hamburger menu

--- no1/script.js
+++ no1/script.js
@@ -1,10 +1,7 @@
-// deklarasi tombol dan menu
+
 const tombol = document.getElementById("hamburger-menu");
 const menu = document.querySelector(".menu");
 
-// membuat event click
-// pada saat tombol di click, tambahkan class aktif pada class menu
-// saat diklik lagi, maka class aktif dihilangkan dari class menu (toggle)
 tombol.addEventListener("click", () => {
   menu.classList.toggle("aktif");
 });
